60df37f1934cc7e4de75f7f0bbcd36f5d83005ff
Richard-CRT <Apples282@hotmail.co.uk>
2015-07-28 11:19:02 +0100

    General Fixes
    Added return to main() in parse.py, added dummy data in order to cut
    down on API calls and improved readability of return dictionary

--- BackEnd/parse.py
+++ BackEnd/parse.py
@@ -5,14 +5,22 @@ import datetime
 
 def main():
     now = datetime.datetime.now()
-    url = 'https://api.forecast.io/forecast/94f336d8e9a37828d0cf3639f474c531/37.8267,-122.423,%s-%s-%sT%s:00:00+01:00?units=si' % (now.year,str(now.month).zfill(2),str(now.day).zfill(2),str(now.hour).zfill(2))
-    response = urllib.request.urlopen(url)
-    str_response = response.readall().decode('utf-8')
+    #url = 'https://api.forecast.io/forecast/94f336d8e9a37828d0cf3639f474c531/37.8267,-122.423,%s-%s-%sT%s:00:00+01:00?units=si&exclude=currently,minutely,daily,alerts,flags' % (now.year,str(now.month).zfill(2),str(now.day).zfill(2),str(now.hour).zfill(2))
+    #response = urllib.request.urlopen(url)
+    #str_response = response.readall().decode('utf-8')
+    file = open("sample data.txt")
+    str_response = file.read()
+    file.close()
     obj = json.loads(str_response)
     ##print('%s-%s-%s %s:%s:%s' % (now.year,str(now.month).zfill(2),str(now.day).zfill(2),str(now.hour).zfill(2),str(now.minute).zfill(2),str(now.second).zfill(2)))
     ##for i in list(obj['currently']):
     ##    print("   " + i)
+    returnList = []
     for i in list(obj['hourly']['data']):
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(i['time'])))
+        tempDictionary = {}
         for x in list(i):
+            tempDictionary[x] = str(i[str(x)])
             print("   " + x + ": " + str(i[str(x)]))
+        returnList.append(tempDictionary)
+    print(returnList)
