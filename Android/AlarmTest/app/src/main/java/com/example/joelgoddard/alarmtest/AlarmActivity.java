b68bb9300ee5968bbf92d85aaf03bd7b0160f968
Joel Goddard <developer@joelgoddard.co.uk>
2015-07-28 10:58:39 +0100

    Added Alarm
    Alarm opens a new new activity when it goes off. Does not wake phone up.

--- Android/AlarmTest/app/src/main/java/com/example/joelgoddard/alarmtest/AlarmActivity.java
+++ Android/AlarmTest/app/src/main/java/com/example/joelgoddard/alarmtest/AlarmActivity.java
@@ -1,36 +1,25 @@
 package com.example.joelgoddard.alarmtest;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class AlarmActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        Intent intent = new Intent(getApplicationContext(), AlarmActivity.class);
-        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(System.currentTimeMillis()+30000, pi), pi);
-        Log.d("Test", "Set Clock");
+        setContentView(R.layout.activity_alarm);
+        Log.d("Test", "Alarm went off");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_alarm, menu);
         return true;
     }
 
